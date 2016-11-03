63a3227e2373a7b7a52229cf9a9b712b56388728
frank <xihuangwutong@gmail.com>
2016-11-03 20:02:53 +0800

    add Session file content

--- framework/web/Session.php
+++ framework/web/Session.php
@@ -12,4 +12,91 @@ class Session extends Component implements \IteratorAggregate, \ArrayAccess,\Cou
     public $flashParam = '__flash';
 
     public $handler;
+
+    private $_cookieParams = ['httpOnly' => true];
+
+    public function init()
+    {
+        parent::init();
+        register_shutdown_function($this, 'close');
+        if ($this->getIsActive()) {
+            Yii::warning("Session is already started", __METHOD__);
+            $this->updateFlashCounters();
+        }
+    }
+
+    public function getUseCustomStorage()
+    {
+        return false;
+    }
+
+    public function open()
+    {
+        if ($this->getIsActive()) {
+            return;
+        }
+
+        $this->registerSessionHandler();
+
+        $this->setCookieParamsInternal();
+
+        @session_start();
+
+        if ($this->getIsActive()) {
+            Yii::info('Session started', __MEHTOD__);
+            $this->updateFlashCounters();
+        } else {
+            $error = error_get_last();
+            $message = isset($error['message']) ? $error['message'] : 'Failed to start session';
+            Yii::error($message, __METHOD__);
+        }
+    }
+
+    protected function registerSessionHandler()
+    {
+        if ($this->handler !== null) {
+            if (!is_object($this->handler)) {
+                $this->handler = Yii::createObject($this->handler);
+            }
+
+            if (!$this->handler instanceof \SessionHandlerInterface) {
+                throw new InvalidConfigException('"' . get_class($this) . '::handler" must implement the SessionHandlerInterface');
+            }
+            @session_set_save_handler($this->handler, false);
+        } else {
+            @session_set_save_handler(
+                [$this, 'openSession'],
+                [$this, 'closeSession'],
+                [$this, 'readSession'],
+                [$this, 'writeSession'],
+                [$this, 'destorySession'],
+                [$this, 'gcSession']
+            );
+        }
+    }
+
+    public function close()
+    {
+        if ($this->getIsActive()) {
+            @session_write_close();
+        }
+    }
+
+    public function destory()
+    {
+        if ($this->getIsActive()) {
+            @session_unset();
+            $sessionId = session_id();
+            @session_destory();
+            @session_id($sessionId);
+        }
+    }
+
+    public function getIsActive()
+    {
+        return session_start() == PHP_SESSION_ACTIVE;
+    }
+
+    private $_hasSessionId;
+
 }
